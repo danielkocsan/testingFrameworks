40fb1540b3cd9c9e294b36f2ab8a7b2e070343e4
Igor Mucsicska <igor_mucsicska@epam.com>
2013-10-29 16:39:20 +0100

    Now its posible to run test without grunt using jasmine-node (Useful for debuggers :) )

--- jasmine/run-tests.js
+++ jasmine/run-tests.js
@@ -1,2 +1,10 @@
-process.chdir('jasmine');
-require('grunt-cli/bin/grunt');
+var fs = require('fs');
+var vm = require('vm');
+var includeInThisContext = function(path) {
+    var code = fs.readFileSync(path);
+    vm.runInThisContext(code, path);
+}.bind(this);
+
+includeInThisContext(__dirname+"/modules/eventPublisher.js");
+
+require('jasmine-node/lib/jasmine-node/cli');
