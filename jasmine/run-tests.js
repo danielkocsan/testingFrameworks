e0e6abfb3c20bc5d7fa07e07adcbe4fad84e20d6
Igor Mucsicska <igor_mucsicska@epam.com>
2013-10-29 16:54:18 +0100

    Process arguments added to jasmine-node runner

--- jasmine/run-tests.js
+++ jasmine/run-tests.js
@@ -7,4 +7,9 @@ var includeInThisContext = function(path) {
 
 includeInThisContext(__dirname+"/modules/eventPublisher.js");
 
+process.argv.push("specs");
+process.argv.push("--junitreport");
+process.argv.push("--output");
+process.argv.push("reports/");
+
 require('jasmine-node/lib/jasmine-node/cli');
